bd67fc13af769f9c5b217a5b3e96522a2eb2e301
Jon Mooney <jon.mooney@yahoo.com>
2022-01-18 13:13:25 -0800

    JS - added global variables and questions object with q and a

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
 //Script file for coding quiz
 
+var score = 0;
+var questionNum = 0;
+var timeInterval;
+var timeLeft = 0;
+
+var questions = [
+    {
+        question: "Which is NOT a commonly used data type: ",
+        option1: "Strings",
+        option2: "Booleans",
+        option3: "Alerts",
+        option4: "Numbers",
+        answer: 3
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ____",
+        option1: "Quotes",
+        option2: "Curly brackets",
+        option3: "Parenthesis",
+        option4: "Square brackets",
+        answer: 3
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____",
+        option1: "Numbers and strings",
+        option2: "Other arrays",
+        option3: "Booleans",
+        option4: "All of the above",
+        answer: 4
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables",
+        option1: "Commas",
+        option2: "Curly brackets",
+        option3: "Quotes",
+        option4: "Parenthesis",
+        answer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        option1: "JavaScript",
+        option2: "Terminal / Bash",
+        option3: "for loops",
+        option4: "console.log",
+        answer: 4
+    }
+];
+
 //Add event listeners after page is loaded
 window.onload = function(){
     var buttonStart = document.querySelector("#button-start");
